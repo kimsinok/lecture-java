42ce218f12aa17c492a53f32ffd5630165b50f73
kso <88135327+kimsinok@users.noreply.github.com>
2024-11-26 14:59:27 +0900

    강의자료입니다.

---
+++ App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args)  {
+        
+        //객체 셍성
+        Car car1 = new Car("소나타", 60);
+
+        System.out.println("car1 = " + car1);
+
+       
+
+    }
+}
