2ed42ac89af9f5f77a7bf33dce96f14bfaec034a
kso <88135327+kimsinok@users.noreply.github.com>
2024-12-03 16:14:30 +0900

    Add files via upload

---
+++ javaApp/src/exception/ExceptionExam2.java
@@ -0,0 +1,19 @@
+package exception;
+
+public class ExceptionExam2 {
+
+    public static void main(String[] args) {
+        
+        String[] fruits = {"Apple", "Graph","Banana"};
+
+        // for (String fruit : fruits) {
+        //     System.out.println(fruit);
+        // }
+
+        for (int i = 0; i < fruits.length; i++) {
+            System.out.println(fruits[i]);
+        }
+
+    }
+
+}
