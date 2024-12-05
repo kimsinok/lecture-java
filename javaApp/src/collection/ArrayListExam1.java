cc3c822a9d559bff9346f0600fd5c80fe3f3ae30
kso <88135327+kimsinok@users.noreply.github.com>
2024-12-05 14:11:10 +0900

    Add files via upload

---
+++ javaApp/src/collection/ArrayListExam1.java
@@ -0,0 +1,26 @@
+package collection;
+
+import java.util.ArrayList;
+
+public class ArrayListExam1 {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> names = new ArrayList<>();
+
+        names.add("kim");
+        names.add("lee");
+        names.add("hong");
+        names.add("park");
+        names.add("moon");
+        // names.add(1); // Error
+
+        // Enhanced for문
+        for(String name : names) {
+            System.out.println(name);
+        }
+        
+    }
+
+}
+
