220677cdbed5bb18809b796d1a52f41853889b48
kso <88135327+kimsinok@users.noreply.github.com>
2024-12-17 12:08:25 +0900

    Add files via upload

---
+++ src/ResultModal.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../ResultModal.css'; // CSS 파일 import
+
+const ResultModal = ({ title, content, callbackFn }) => {
+    return (
+        <div className="modal-overlay">
+            <div className="modal-container">
+                <div className="modal-header">
+                    <h2>{title}</h2>
+                </div>
+                <div className="modal-content">
+                    <p>{content}</p>
+                </div>
+                <div className="modal-footer">
+                    <button className="close-button" onClick={() => { if (callbackFn) { callbackFn(); } }}>
+                        Close Modal
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ResultModal;
